2da9833c5d823ba5b5a18dc22bbc28e092fcf2b4
Claudia Herrera <astrologiaclh@gmail.com>
2023-08-05 20:55:20 -0300

    agregando funciones javascript

--- pages/main.js
+++ pages/main.js
@@ -1,16 +1,58 @@
 //Se solicita año, mes y dia de nacimiento. Se restan los valores en ese orden, se llega a una cifra de cuatro digitos.
 //luego debe separarse esa cifra en digitos y sumarse hasta obtener un solo dígito. Y con ese valor se devuelve una interpretación segun el número. 
-let año = parseInt(promp("Ingresa tu año de nacimiento con números"));
-if (año >= 1900 && año <= 2023)
-alert (`año de nacimiento ${año}`);
-else {"Ingresa un año entre 1900 y 2023"}
 
-let mes = parseInt(promp("Ingresa tu mes de nacimiento con números"));
-if (mes >= 1 && mes <= 12)
-alert (`mes de nacimiento ${mes}`);
-else {"Ingresa un mes entre 1 y 12"}
+let num1 = parseInt(prompt("Ingrese año de nacimiento en números"));
+if (num1 >= 1900 && num1 <= 2023) {//
+    alert(`año de nacimiento ${num1}`);
+} else {//
+    alert("ingresa un año entre 1900 y 2023");
+    let num1 = parseInt(prompt("Ingrese año de nacimiento en números"));
+}
 
-let dia = parseInt(promp("Ingresa tu dia de nacimiento con números"));
-if (dia >= 1 && dia <= 31)
-alert (`dia de nacimiento ${dia}`);
-else {"Ingresa un dia entre 1 y 31"}
+let num2 = parseInt(prompt("Ingresa mes de nacimiento en números"));
+if (num2 >= 1 && num2 <= 12) {//
+    alert(`mes de nacimiento ${num2}`);
+} else {
+    alert("ingresa un mes entre 1 y 12");
+    let num2 = parseInt(prompt("Ingresa mes de nacimiento en números"));
+}
+
+let num3 = parseInt(prompt("Ingresa día de nacimiento en números"));
+if (num3 >= 1 && num3 <= 31) {
+    alert(`día de nacimiento ${num3}`);
+} else {
+    alert("ingresa un día entre 1 y 31");
+    let num3 = parseInt(prompt("Ingresa día de nacimiento en números"));
+}
+alert(`Tu año karmico es ${num1 - num2 - num3}`);
+
+// Noto que si se pone un valor fuera de parámetro y se corrige toma como valor el primero que se colocó y hace mal la cuenta.
+// Dejo la función que aplicaría para este cálculo.
+
+//const restar = (num1, num2, num3) => num1 - num2 - num3;
+
+
+
+// Mes promocional en los cursos de Numerología, descuento 15%. 
+
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const descuento = (x) => x * porcentaje / 100;
+
+let precioCurso = 12000;
+let porcentaje = 15;
+
+let totalCurso = suma(resta(precioCurso, descuento(precioCurso)), porcentaje);
+
+console.log(totalCurso);
+
+
+// preinscripcion para un curso con 20 vacantes 
+
+for (let vacante = 1; vacante <= 20; vacante++) {
+    let nombre = prompt("Ingresa tu nombre y Apellido");
+    alert(`Vancante #${vacante} Nombre: ${nombre}`);
+}
+alert("Ya no quedan vacantes para este curso");
+
+
